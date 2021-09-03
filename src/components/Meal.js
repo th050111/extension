bf34eab168df509a0443a012a3b390a7f8bd1ca5
tgim4253 <tgim4253@pess.cnehs.kr>
2021-09-03 11:23:17 +0900

    1.0

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -5,12 +5,22 @@ import { authService, dbService } from "../myBase";
 
 function Meal() {
 	const [date,setDate] = useState({
-		month:9,
-		
 	});
+	const [today,setToday] = useState({});
 	useEffect(()=>{
-		const date = new Date().now().getDate();
-		const month = new Date().now().getMonth();
+		
+		
+		const date = new Date().getDate();
+		const month = new Date().getMonth()+1;
+		console.log(date,month)
+		setDate({
+			month:month,
+			date: date,
+		})
+		setToday({
+			month:month,
+			date: date,
+		})
 	},[])
 	
   return (
